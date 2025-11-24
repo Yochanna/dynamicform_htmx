ca85e47f36972fa6f7b11b754bd336a64b159082
Jane <razwiedanijane@gmail.com>
2025-11-24 20:49:18 +0200

    Allow Render host in ALLOWED_HOSTS

--- django_dynamic_form_tutorial1/settings.py
+++ django_dynamic_form_tutorial1/settings.py
@@ -4,7 +4,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'demo-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'dynamicform-htmx.onrender.com',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
